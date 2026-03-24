fd28345d95cf2f67b6a5390bf891c8bbe156123f
fabiocatone <fabbiocatone@email.com>
2026-03-24 13:36:58 +0100

    finish snack, is a good snack

--- snack 2/main.js
+++ snack 2/main.js
@@ -1,25 +1,34 @@
-function random(){
-    Math.floor(Math.random()*100)
-
+function random(max,min){
+    return Math.floor(Math.random() * (max - min) ) + min;
 }
 
 const league = [
     {
         "name" : "Internazionale",
-        "points" : random(),
-        "fouls-suffered" : random()
+        "points" : Number(random(100,1)),
+        "fouls-suffered" : Number(random(100,1))
     }, 
 
     {
         "name" : "A.C Milan",
-        "points" : random (),
-        "fouls-suffered" : random()
+        "points" : Number(random(100,1)),
+        "fouls-suffered" : Number(random(100,1))
     },
     
     {"name" : "Napoli",
-        "points" : random(),
-        "fouls-suffered" : random()}
+        "points" : Number(random(100,1)),
+        "fouls-suffered" : Number(random(100,1))}
 
     
 ]
 console.log(league);
+
+let fixArray = []
+for (let i = 0; i<league.length; i++) {
+   delete league[i].points
+   fixArray.push(league[i])
+}
+
+console.log(fixArray);
+
+
